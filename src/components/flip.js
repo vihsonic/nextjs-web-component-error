2a336ca8d62019a687d575f1569ba87b86749d1c
black02430horse <black02430horse@gmail.com>
2023-04-16 17:54:30 +0200

    clean

---
+++ src/components/flip.js
@@ -0,0 +1,5 @@
+import "@/webComponents/my-flip.js";
+export default function DynamicFlip() {
+    return <my-flip url={`https://3vc.s3.amazonaws.com/research/heroes-of-mavia/`} amount={9}></my-flip>
+
+}
